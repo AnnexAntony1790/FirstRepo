1ab2fd2a38ed6cb492709692a33d6574aa4f15b9
anexr <anexr@HP>
2024-11-22 11:59:51 +0530

    My first Commit

---
+++ firstmavenproject/src/main/java/firstmavenpackage/AdditionEx.java
@@ -0,0 +1,18 @@
+package firstmavenpackage;
+
+public class AdditionEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AdditionEx obj=new AdditionEx();
+		obj.add();
+
+	}
+	public void add()
+	{
+		int a=10;int b=20;int c;
+		c=a+b;
+		System.out.println("Sum:"+c);
+	}
+}
