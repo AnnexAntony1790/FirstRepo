f46a13643b28266f61aea25df287bb500fb57caf
anexr <anexr@HP>
2024-11-22 12:07:54 +0530

    Finding difference

---
+++ firstmavenproject/src/main/java/firstmavenpackage/Difference.java
@@ -0,0 +1,18 @@
+package firstmavenpackage;
+
+public class Difference {
+	public void difference()
+	{
+		int a=100;int b=20;int c;
+		c=a-b;
+		System.out.println("Difference:"+c);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Difference obj=new Difference();
+		obj.difference();
+
+	}
+
+}
