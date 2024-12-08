38b9216368137a601bad4040c9c2f8cc3f1cf506
anexr <anexr@HP>
2024-12-08 23:06:45 +0530

    Encapsulation Example

---
+++ firstmavenproject/src/main/java/encapsulation/EncapsulationEx2.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class EncapsulationEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EncapsulationEx1 obj=new EncapsulationEx1();
+		obj.setdetails(23, "Anu");
+		obj.getdetails();
+	}
+
+}
